a82052df401c41db9fe8fdb9b3bab5edffc6c960
Rohan <rohanshakya254@gmail.com>
2020-10-30 21:54:03 +0545

    created links page

---
+++ datas.js
@@ -0,0 +1,26 @@
+export const datas = [
+  {
+    id: 1,
+    title: 'Natours Project',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/advanced-css-and-sass/tree/master/project-1',
+    demo:
+      'https://rohan-shakya.github.io/advanced-css-and-sass/project-1/index.html',
+  },
+  {
+    id: 2,
+    title: 'Trillo Project',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/advanced-css-and-sass/tree/master/project-2',
+    demo:
+      'https://rohan-shakya.github.io/advanced-css-and-sass/project-2/index.html',
+  },
+  {
+    id: 3,
+    title: 'Nexter Project',
+    sourceCode:
+      'https://github.com/Rohan-Shakya/advanced-css-and-sass/tree/master/project-3',
+    demo:
+      'https://rohan-shakya.github.io/advanced-css-and-sass/project-3/index.html',
+  },
+];
